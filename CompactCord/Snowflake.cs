ed930fb218a0029761083f347934a6c0b6b7619c
ElijahZAwesome <>
2019-09-30 21:15:41 -0500

    Snowflake dummy constructors
    Also, I seem to have bad wordedness

--- CompactCord/Snowflake.cs
+++ CompactCord/Snowflake.cs
@@ -15,6 +15,11 @@ namespace CompactCord {
 		public static DateTime TimeStamp;
 		public static float TimeStampAsEpoch;
 
+		/// <summary>
+		/// This constructor constructs a Snowflake object from an EXISTING SNOWFLAKE STRING.
+		/// Use this in conjunction with Discord's API.
+		/// </summary>
+		/// <param name="input">A snowflake ID string.</param>
 		public Snowflake(string input)
 		{
 			OriginalID = input;
@@ -29,6 +34,26 @@ namespace CompactCord {
 			TimeStampAsEpoch = epochTime;
 			DateTime dateTime = DateTime.Parse(epochTime.ToString());
 			TimeStamp = dateTime;
+		}
+
+		/// <summary>
+		/// Generates a new Snowflake object from a passed epoch time (will subtract discord epoch time)
+		/// </summary>
+		/// <param name="epoch">A Unix Epoch time to pass.</param>
+		public Snowflake(float epoch)
+		{
+			// Not really sure how im supposed to get the processor/worker IDs so just gonna stub this for now
+			// timestamp_ms - DISCORD_EPOCH << 22
+		}
+
+		/// <summary>
+		/// Generates a new Snowflake object using a DateTime object (subtracts the discord epoch time)
+		/// </summary>
+		/// <param name="timeStamp">Timestamp used to create the snowflake.</param>
+		public Snowflake(DateTime timeStamp)
+		{
+			// Not really sure how im supposed to get the processor/worker IDs so just gonna stub this for now
+			// timestamp_ms - DISCORD_EPOCH << 22
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CompactCord
-An attempt to make a Discord Bot/User API in C# for Windows CE/Windows 2003-SE/etc
+An attempt to make a Discord Bot/User API Wrapper in C# for Windows CE/Windows 2003-SE/etc
 
 Since I'm using .NET Compact Framework, a lot of crap doesn't work. So, don't be surprised if there's some dumb code workarounds.
 
